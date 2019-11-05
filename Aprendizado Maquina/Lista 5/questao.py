8dfb8cde14888a9c5bec221b06d5a50d31165db2
Joyce Brum <joycebrumu.u@gmail.com>
2019-11-04 23:08:55 -0300

    ML lista 5

--- Aprendizado Maquina/Lista 5/questao.py
+++ Aprendizado Maquina/Lista 5/questao.py
@@ -7,21 +7,35 @@ y1 = [ [39300], [60000], [68500], [86000], [113000], [133000] ]
 
 entrada1 = [848, 912]
 
-def modelo1():
-    global x1
-    global y1
+def regressao(X, y):
+    return matmul(matmul(inv(matmul(X.transpose(), X)),X.transpose()), y)
+
+def Q1_modelo1(d, y):
     X = []
-    for v in x1:
+    for v in d:
         X.append([1, v])
     X = array(X)
-    return matmul(matmul(inv(matmul(X.transpose(), X)),X.transpose()), y1)
+    return regressao(X, y)
 
-def modelo2():
-    global x1
-    global y1
+def Q1_modelo2(d, y):
     X = []
+    for v in d:
+        X.append([1, v, pow(v,2), pow(v,3), pow(v,4)])
+    X = array(X)
+    return regressao(X, y)
+
+def Q1_modelo1_erro():
+    X = []
+    for v in d:
+        X.append([1, v])
+    X = array(X)
+    
+    w = regressao(X, y1)
+    somatorio = 0
     for v in x1:
-        X.append([1, ])
+        m = array([1, v])
+        v = matmul(m, w)
+        somatorio = pow(v - )
     
 
     
