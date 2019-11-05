5c1e0d1516c59abf4fdff59cf728e8eb39c1f4aa
Joyce Brum <joycebrumu.u@gmail.com>
2019-11-04 22:47:32 -0300

    terminado o trabalho do lamda simplificado

--- Computação 3/Lamda/simplificada.cc
+++ Computação 3/Lamda/simplificada.cc
@@ -1,115 +1,109 @@
 #include <iostream>
+#include <vector>
+#include <type_traits>
+#include <functional>
+#include <string>
+#include <stdio.h>
 using namespace std; 
 
 auto x = []( auto v ){ return v; };
-auto cte( auto valor ) { return [valor]( auto x ) { return valor; } ; }
+constexpr auto cte( auto valor ) { return [valor]( auto x ) { return valor; } ; }
 
-// --------------- operador Soma
+template<typename Op>
+constexpr auto getValue(Op v) {
+	if constexpr (is_arithmetic<Op>::value || is_same<Op, string>:: value) {
+		return cte(v);
+	}
+	else {
+		return v;
+	}
+}
 
 template <typename Op1, typename Op2>
-auto operator + ( Op1 a, Op2 b ) {
+auto operator + ( Op1 v1, Op2 v2 ) {
+	auto a = getValue(v1);
+	auto b = getValue(v2);
 	return [a,b]( auto v ){ return a(v) + b(v); }; 
 }
-template <typename Op1>
-auto operator + ( Op1 a, int valor ) {
-	auto b = cte(valor);
-	return [a,b]( auto v ){ return a(v) + b(v); }; 
+template <typename Op1, typename Op2>
+auto operator - ( Op1 v1, Op2 v2 ) {
+	auto a = getValue(v1);
+	auto b = getValue(v2);
+	return [a,b]( auto v ){ return a(v) - b(v); }; 
 }
-template <typename Op2>
-auto operator + ( int a, Op2 valor ) {
-	auto b = cte(valor);
-	return [a,b]( auto v ){ return a(v) + b(v); }; 
+template <typename Op1, typename Op2>
+auto operator * ( Op1 v1, Op2 v2 ) {
+	auto a = getValue(v1);
+	auto b = getValue(v2);
+	return [a,b]( auto v ){ return a(v) * b(v); }; 
 }
-template <typename Op1>
-auto operator + ( Op1 a, double valor ) {
-	auto b = cte(valor);
-	return [a,b]( auto v ){ return a(v) + b(v); }; 
+template <typename Op1, typename Op2>
+auto operator / ( Op1 v1, Op2 v2 ) {
+	auto a = getValue(v1);
+	auto b = getValue(v2);
+	return [a,b]( auto v ){ return a(v) / b(v); }; 
 }
-template <typename Op2>
-auto operator + ( double a, Op2 valor ) {
-	auto b = cte(valor);
-	return [a,b]( auto v ){ return a(v) + b(v); }; 
+template <typename Op1, typename Op2>
+auto operator % ( Op1 v1, Op2 v2 ) {
+	auto a = getValue(v1);
+	auto b = getValue(v2);
+	return [a,b]( auto v ){ return a(v) % b(v); };
+} 
+template <typename Op1, typename Op2>
+auto operator == ( Op1 v1, Op2 v2 ) {
+	auto a = getValue(v1);
+	auto b = getValue(v2);
+	return [a,b]( auto v ){ return a(v) == b(v); };
+} 
+template <class Op1, typename Op2>
+auto operator << ( Op1 v1, Op2 v2 ) {
+	auto a = getValue(v1);
+	auto b = getValue(v2);
+	return [a,b]( auto v ){ a(v); cout << b(v); };
+} 
+template <class Op2>
+auto operator << ( ostream& o, Op2 v2 ) {
+	auto b = getValue(v2);
+	return [b]( auto v ){ cout << b(v); };
 }
 
-// ------------------ operador Subtração
-template <typename Op1, typename Op2>
-auto operator - ( Op1 a, Op2 b ) {
-	return [a,b]( auto v ){ return a(v) - b(v); }; 
+ostream& operator << ( ostream& o, char c) {
+	printf("%c", c);
+	return o;
 }
-template <typename Op1>
-auto operator - ( Op1 a, int valor ) {
-	auto b = cte(valor);
-	return [a,b]( auto v ){ return a(v) - b(v); }; 
+ostream& operator << ( ostream& o, string s) {
+	const char *c = s.c_str();
+	printf("%s", c);
+	return o;
 }
-template <typename Op2>
-auto operator - ( int a, Op2 valor ) {
-	auto b = cte(valor);
-	return [a,b]( auto v ){ return a(v) - b(v); }; 
-}
-template <typename Op1>
-auto operator - ( Op1 a, double valor ) {
-	auto b = cte(valor);
-	return [a,b]( auto v ){ return a(v) - b(v); }; 
-}
-template <typename Op2>
-auto operator - ( double a, Op2 valor ) {
-	auto b = cte(valor);
-	return [a,b]( auto v ){ return a(v) - b(v); }; 
-}
+template <typename V, typename F>
+auto operator | ( const V& v, F func ) {
+    if constexpr( is_same_v< bool, invoke_result_t< F, decltype( *begin( v ) ) > > ) {
+        vector< decay_t< decltype( *begin( v ) ) > > res;
+ 
+	for( auto x: v )
+            if( invoke( func, x ) )
+                res.push_back( x );
 
-// ------------------ operador Multiplicação
-template <typename Op1, typename Op2>
-auto operator * ( Op1 a, Op2 b ) {
-	return [a,b]( auto v ){ return a(v) * b(v); }; 
-}
-template <typename Op1>
-auto operator * ( Op1 a, int valor ) {
-	auto b = cte(valor);
-	return [a,b]( auto v ){ return a(v) * b(v); }; 
-}
-template <typename Op2>
-auto operator * ( int a, Op2 valor ) {
-	auto b = cte(valor);
-	return [a,b]( auto v ){ return a(v) * b(v); }; 
-}
-template <typename Op1>
-auto operator * ( Op1 a, double valor ) {
-	auto b = cte(valor);
-	return [a,b]( auto v ){ return a(v) * b(v); }; 
-}
-template <typename Op2>
-auto operator * ( double a, Op2 valor ) {
-	auto b = cte(valor);
-	return [a,b]( auto v ){ return a(v) * b(v); }; 
-}
-
-// ------------------ operador Divisão
-template <typename Op1, typename Op2>
-auto operator / ( Op1 a, Op2 b ) {
-	return [a,b]( auto v ){ return a(v) / b(v); }; 
-}
-template <typename Op1>
-auto operator / ( Op1 a, int valor ) {
-	auto b = cte(valor);
-	return [a,b]( auto v ){ return a(v) / b(v); }; 
-}
-template <typename Op2>
-auto operator / ( int a, Op2 valor ) {
-	auto b = cte(valor);
-	return [a,b]( auto v ){ return a(v) / b(v); }; 
-}
-template <typename Op1>
-auto operator / ( Op1 a, double valor ) {
-	auto b = cte(valor);
-	return [a,b]( auto v ){ return a(v) / b(v); }; 
-}
-template <typename Op2>
-auto operator / ( double a, Op2 valor ) {
-	auto b = cte(valor);
-	return [a,b]( auto v ){ return a(v) / b(v); }; 
+        return res;
+    }
+    else if constexpr( is_same_v< void, invoke_result_t< F, decltype( *begin( v ) ) > > ) {
+        for( auto x: v )
+            func( x );
+    }
+    else {
+        vector< decay_t< invoke_result_t< F, decltype( *begin( v ) ) > > > res;
+        
+        for( auto x: v )
+            res.push_back( invoke( func, x ) );
+            
+        return res;
+    }  
 }
 
 int main() {
-	auto s = x + x + 7 - 1;
-	cout << s(8) << endl;
+	  
+	string v1[] = { "a", "b2", "cc3", "saci" };
+	v1 | []( string n ) { return n.length() % 2 == 0; } | (x+x) | cout << x << ' ';
+
 }
