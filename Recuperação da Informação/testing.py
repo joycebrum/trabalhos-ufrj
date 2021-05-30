9ae946b1372889ba21bb88cd4b7a70a9e7d84563
joycebrum <joycebrumu.u@gmail.com>
2021-05-30 19:40:17 -0300

    RecInfo - trying to connect with neo4j

--- Recuperação da Informação/testing.py
+++ Recuperação da Informação/testing.py
@@ -1,6 +1,7 @@
 from neo4j import GraphDatabase
+from bottle import get, run, request, response, static_file
 
-class HelloWorldExample:
+class Example:
 
     def __init__(self, uri, user, password):
         self.driver = GraphDatabase.driver(uri, auth=(user, password), encrypted=False)
@@ -8,20 +9,13 @@ class HelloWorldExample:
     def close(self):
         self.driver.close()
 
-    def print_greeting(self, message):
-        with self.driver.session() as session:
-            greeting = session.write_transaction(self._create_and_return_greeting, message)
-            print(greeting)
-
-    @staticmethod
-    def _create_and_return_greeting(tx, message):
-        result = tx.run("CREATE (a:Greeting) "
-                        "SET a.message = $message "
-                        "RETURN a.message + ', from node ' + id(a)", message=message)
-        return result.single()[0]
+    @get("/")
+    def get_index(self):
+        return static_file("index.html", root="static")
 
 
 if __name__ == "__main__":
-    greeter = HelloWorldExample("bolt://localhost:7687", "neo4j", "123456")
-    # greeter.print_greeting("hello, world")
-    greeter.close()
+    example = Example("bolt://localhost:7687", "neo4j", "130598")
+    print(example.get_index())
+    print("Sucesso")
+    example.close()
