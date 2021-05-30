4b6f8c2ae5b6cdfba7f953deb55b3fc087a0a6a2
Joyce Brum <joycebrumu.u@gmail.com>
2021-05-30 19:32:37 -0300

    sei la - projeto de teste

--- Projeto de testes/testTestes.py
+++ Projeto de testes/testTestes.py
@@ -6,5 +6,8 @@ class TestMethods(unittest.TestCase):
     def test_twelve(self):
         assert test.between(12, 00) == 0
 
+    def test_three(self):
+        assert test.between(3, 00) == 0
+
 suite = unittest.TestLoader().loadTestsFromTestCase(TestMethods)
 unittest.TextTestRunner().run(suite)
