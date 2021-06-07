dedc6a3bbfc73c42299197dd3fe96ecc3526c082
joycebrum <joycebrumu.u@gmail.com>
2021-06-06 23:37:45 -0300

    starting to think on using relevance

--- Recuperação da Informação/project/bm25.py
+++ Recuperação da Informação/project/bm25.py
@@ -4,6 +4,25 @@ import math
 
 K1 = 1
 b = 0.75
+def init_search (query_string):
+    query = stemm.pre_process(query_string)
+    documents = db.all_musics()
+    N = db.number_of_documents()
+    avg_doclen = db.total_occurrences()/N
+    n = [db.number_documents_with_term(q) for q in query]
+    return query, documents, N, avg_doclen, n
+
+def get_relevance_document_rank(N, n, avg_doclen, doc, query):
+    #to do think a way to use the relevance on BM25
+    print('do nothing')
+    return 0
+
+def search_with_relevance(query_string):
+    query, documents, N, avg_doclen, n = init_search(query_string)
+    for document in documents:
+        doclen = db.document_len(document['id'])
+        document['rank'] = get_relevance_document_rank(N, n, avg_doclen, {'len': doclen, 'id': document['id']}, query)
+    return sorted(documents, key=lambda k: k['rank'], reverse=True)
 
 def get_document_rank(N, n, avg_doclen, doc, query):
     soma = 0
@@ -16,12 +35,9 @@ def get_document_rank(N, n, avg_doclen, doc, query):
         soma += Bij * math.log(pre_log_value, 2)
         i += 1
     return soma
+
 def search(query_string):
-    query = stemm.pre_process(query_string)
-    documents = db.all_musics()
-    N = db.number_of_documents()
-    avg_doclen = db.total_occurrences()/N
-    n = [db.number_documents_with_term(q) for q in query]
+    query, documents, N, avg_doclen, n = init_search(query_string)
     for document in documents:
         doclen = db.document_len(document['id'])
         document['rank'] = get_document_rank(N, n, avg_doclen, {'len': doclen, 'id': document['id']}, query)
@@ -31,4 +47,3 @@ if __name__ == "__main__":
     res = search('lov story')
     for r in res:
         print(r['name'], r['rank'])
-        
